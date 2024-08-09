ed323df642aa98c355b3950b61a1279aab19fcde
ShadmanKhan10 <khanshadman7777@gmail.com>
2024-08-09 10:27:57 +0530

    Started learning for 9th August 2024

--- learn-react-here/src/App.jsx
+++ learn-react-here/src/App.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [file, setFile] = useState();
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log("File Name: ", file.name);
+  };
   return (
     <>
-      <h1>Start learning here</h1>
+      {" "}
+      <form onSubmit={handleSubmit}>
+        <h1>React File Upload</h1>
+        <input type="file" onChange={handleFileChange} />
+        <button type="submit">Upload</button>
+        <img src={file} alt="file" />
+      </form>
     </>
   );
 }
