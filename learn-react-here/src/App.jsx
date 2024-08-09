99e6e6829baf9d367faa63090eff8581aeb19b50
ShadmanKhan10 <khanshadman7777@gmail.com>
2024-08-09 12:44:04 +0530

    Learned about props

--- learn-react-here/src/App.jsx
+++ learn-react-here/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import Practice1 from "./Components/Practice1";
+import Practice2 from "./Components/Practice2";
 
 function App() {
   return (
     <>
       <Practice1 />
+      <Practice2 display={true} />
     </>
   );
 }
---
+++ learn-react-here/src/Components/Practice2.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Practice2({ display }) {
+  return <div>{display ? <h1>Welcome</h1> : <h1>Login to Enter</h1>}</div>;
+}
